d7f6d244ba7cd652ff18fd4379cb384b9672aaca
Daniel Ricardo <dricardo@stp.gov.py>
2015-09-17 18:18:19 -0400

    actualizacion

--- src/main/java/py/gov/stp/tools2/ConnectionConfiguration.java
+++ src/main/java/py/gov/stp/tools2/ConnectionConfiguration.java
@@ -8,19 +8,12 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-<<<<<<< HEAD
 import py.gov.stp.tools.SqlHelper;
 
 public class ConnectionConfiguration {
     public static final String URL = "jdbc:mysql01://pg01.stp.goy.py:3306/spr?useUnicode=true&characterEncoding=UTF-8";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "cybiraconsulting";
-=======
-public class ConnectionConfiguration {
-    public static final String URL = "jdbc:postgres://mysql01.stp.goy.py:3306/spr?useUnicode=true&characterEncoding=UTF-8";
-    public static final String USERNAME = "bpm";
-    public static final String PASSWORD = "cybiraconsulting2";
->>>>>>> 22f7f2500b5e74445483fd7feaff463adcc95484
  
     public static Connection getConnection() {
         Connection connection = null;
@@ -40,79 +33,15 @@ public class ConnectionConfiguration {
 	        try {Class.forName("org.postgresql.Driver");}
 	        catch (ClassNotFoundException e) {e.printStackTrace();}
 	        String url = "";
-<<<<<<< HEAD
-	        String user = "bpm";
-	        String password = "cybiraconsulting";
-
-	        try {con = DriverManager.getConnection("jdbc:postgresql://pg02test.stp.gov.py/stp_crm2?useUnicode=true&characterEncoding=UTF-8&user=bpm&password=cybiraconsulting");}
-	        catch (SQLException ex) {
-	            Logger lgr = Logger.getLogger(SqlHelper.class.getName());
-	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
-	        } 
-	        return con;
-	}
-    public static Connection conectarBase(){
-		 Connection con = null;
-	        Statement st = null;
-	        ResultSet rs = null;
-	        try {Class.forName("org.postgresql.Driver");}
-	        catch (ClassNotFoundException e) {e.printStackTrace();}
-	        String url = "";
-	        String user = "bpm";
-	        String password = "cybiraconsulting2";
-
-	        try {con = DriverManager.getConnection("jdbc:postgresql://pg02.stp.gov.py/base?useUnicode=true&characterEncoding=UTF-8&user=bpm&password=cybiraconsulting2");}
-	        catch (SQLException ex) {
-	            Logger lgr = Logger.getLogger(SqlHelper.class.getName());
-	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
-	        } 
-	        return con;
-	}
-    public static Connection conectarSpr(){
-		 Connection con = null;
-	        Statement st = null;
-	        ResultSet rs = null;
-	        try {Class.forName("com.mysql.jdbc.Driver");}
-	        catch (ClassNotFoundException e) {e.printStackTrace();}
-	        String url = "";
-	        String user = "root";
-	        String password = "t3R3R3.ol";
-
-	        try {con = DriverManager.getConnection("jdbc:mysql://mysql01.stp.gov.py/spr?useUnicode=true&characterEncoding=UTF-8", "root", "t3R3R3.ol");}
-=======
 	        String user = "postgres";
 	        String password = "t3R3R3.ol";
 
 	        try {con = DriverManager.getConnection("jdbc:postgresql://pg01.stp.gov.py/tablero2015v3?useUnicode=true&characterEncoding=UTF-8&user=postgres&password=t3R3R3.ol");}
->>>>>>> 22f7f2500b5e74445483fd7feaff463adcc95484
-	        catch (SQLException ex) {
-	            Logger lgr = Logger.getLogger(SqlHelper.class.getName());
-	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
-	        } 
-<<<<<<< HEAD
-	        return con;
-	}
-    public static Connection conectarSnpp(){
-		 Connection con = null;
-	        Statement st = null;
-	        ResultSet rs = null;
-	        try {Class.forName("com.mysql.jdbc.Driver");}
-	        catch (ClassNotFoundException e) {e.printStackTrace();}
-	        String url = "";
-	        String user = "root";
-	        String password = "t3R3R3.ol";
-
-	        try {con = DriverManager.getConnection("jdbc:mysql://mysql01.stp.gov.py/tablero2015?useUnicode=true&characterEncoding=UTF-8", "root", "t3R3R3.ol");}
 	        catch (SQLException ex) {
 	            Logger lgr = Logger.getLogger(SqlHelper.class.getName());
 	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
 	        } 
 	        return con;
 	}
-=======
-	        return con; 
-	}
 
->>>>>>> 22f7f2500b5e74445483fd7feaff463adcc95484
- 
-}
+	}
