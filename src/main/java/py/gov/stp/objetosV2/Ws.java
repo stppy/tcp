06f01d575122255cc9f97b71ac5b09677cc0df8b
Rodrigo Zarate <rzarate@stp.gov.py>
2015-09-25 15:14:47 -0400

    Falta que mirian levante sus cambios y tambien dani

--- src/main/java/py/gov/stp/objetosV2/Ws.java
+++ src/main/java/py/gov/stp/objetosV2/Ws.java
@@ -11,7 +11,7 @@ public class Ws {
 	protected String idClave;
 	protected String idValor;
 	protected int wsTipoId;
-	protected boolean borrado;
+	protected boolean borrado=false;
 	
 	public int getId() {
 		return id;
