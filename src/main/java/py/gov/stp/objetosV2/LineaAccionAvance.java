5e8f51e317e6b301298698bbd7fb3ab4db2df7d7
mmarecos <mmarecos@stp.gov.py>
2016-04-19 15:27:23 -0400

    vista resumen linea accion avance del tablero

---
+++ src/main/java/py/gov/stp/objetosV2/LineaAccionAvance.java
@@ -0,0 +1,21 @@
+package py.gov.stp.objetosV2;
+
+public class LineaAccionAvance extends ResumenLineaAccion{
+	
+	protected int insLineaAccionId;
+	protected double cantidad;
+	
+	public int getInsLineaAccionId() {
+		return insLineaAccionId;
+	}
+	public void setInsLineaAccionId(int insLineaAccionId) {
+		this.insLineaAccionId = insLineaAccionId;
+	}
+	public double getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
---
+++ src/main/java/py/gov/stp/objetosV2/LineaAccionProgramacion.java
@@ -0,0 +1,28 @@
+package py.gov.stp.objetosV2;
+
+public class LineaAccionProgramacion extends LineaAccionAvance {
+	
+	protected int insLineaAccionId;
+	protected double cantidadAnho;
+	protected double cantidadHoy;
+	
+	public int getInsLineaAccionId() {
+		return insLineaAccionId;
+	}
+	public void setInsLineaAccionId(int insLineaAccionId) {
+		this.insLineaAccionId = insLineaAccionId;
+	}
+	public double getCantidadAnho() {
+		return cantidadAnho;
+	}
+	public void setCantidadAnho(double cantidadAnho) {
+		this.cantidadAnho = cantidadAnho;
+	}
+	public double getCantidadHoy() {
+		return cantidadHoy;
+	}
+	public void setCantidadHoy(double cantidadHoy) {
+		this.cantidadHoy = cantidadHoy;
+	}
+
+}
