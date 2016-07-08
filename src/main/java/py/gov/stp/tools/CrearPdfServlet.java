b96407df923dc8463c5e846645802128c2da78a4
Alcides Abel Benitez <aabenitez@stp.gov.py>
2016-07-08 17:59:03 -0400

    Tarea #9181: Funcionando Generación de Avance cualitativo por institución. Faltan Ajustes de Estilo.

--- src/main/java/py/gov/stp/tools/CrearPdfServlet.java
+++ src/main/java/py/gov/stp/tools/CrearPdfServlet.java
@@ -1,7 +1,5 @@
 package py.gov.stp.tools;
  
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.lowagie.text.DocumentException;
 
 import java.io.IOException;
@@ -21,6 +19,8 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 import py.gov.stp.objetosV2.Accion;
 import py.gov.stp.objetosV2.AccionCatalogo;
 import py.gov.stp.objetosV2.AvanceCualitativo;
+import py.gov.stp.objetosV2.InsLineaAccion;
+import py.gov.stp.objetosV2.LineaAccion;
 import py.gov.stp.objetosV2.Trimestre;
 import py.gov.stp.tools2.SqlSelects;
  
@@ -148,6 +148,7 @@ public class CrearPdfServlet extends HttpServlet {
         return "Este Servlet Genera PDF Usando librería de Flying Saucer.";
     }	
     
+    
     public String obtenerContenidoAvances(String idAvanceCualitativo, String institucion) {
     
 		String contenido = "";
@@ -188,110 +189,77 @@ public class CrearPdfServlet extends HttpServlet {
    							
 				    	 "</div>";
     	}
-    	else {    		    		
-    		contenido += "<br><h1 class='text-center'><u>SPR-PA-03: Informe de Avance Cualitativo por Institución para el Periodo 2016 </u></h1>";
+    	else {
+    		String periodoAct = "2016";
     		
-    		// verificar periodo en los avances!! Esto se debe implementar..
+    		contenido += "<h1 style='text-align:center;'><u>SPR-PA-03: Informe de Avance Cualitativo por Institución para el Periodo "+periodoAct+" </u></h1>";
     		
-    		condition = " where true and borrado=false";                    
-			List<AvanceCualitativo> avanceCualitativo=null;						        
-			try {avanceCualitativo = SqlSelects.selectAvanceCualitativo(condition);}
+    		condition = " where true and borrado is false ";
+			List<LineaAccion> lineaAccion=null; 		
+			try {lineaAccion = SqlSelects.selectLineaAccion(condition);}
 			catch (SQLException e) {e.printStackTrace();}
-    		
-			Iterator<AvanceCualitativo> avanceCualitativoIterador = avanceCualitativo.iterator();
-			while (avanceCualitativoIterador.hasNext()) {
-				AvanceCualitativo avanceCualitativoAux = avanceCualitativoIterador.next();											
-					
-				condition = " where true ";
-				Integer catalogoAccionId = avanceCualitativoAux.getAccionCatalogoId();
-				List<AccionCatalogo> catalogo=null; 		
-				if (catalogoAccionId!=null) {condition += " and id ='"+catalogoAccionId+"'";
-				try {catalogo = SqlSelects.selectAccionCatalogo(condition);}
+			
+	    	Iterator<LineaAccion> lineaAccionIterador = lineaAccion.iterator();
+			while (lineaAccionIterador.hasNext()) {
+				LineaAccion lineaAccionAux = lineaAccionIterador.next();
+				
+				contenido += "<h2 class='text-center'><u>Linea de Acción: "+ lineaAccionAux.getNombre() +" </u></h2>";
+    		    		 
+				condition = " where true and borrado is false and periodo_id="+ periodoAct +" ";
+				Integer lineaAccionId = lineaAccionAux.getId();
+				List<InsLineaAccion> insLineaAccion=null; 		
+				if (lineaAccionId != null) {condition += " and linea_accion_id ='"+lineaAccionId+"'";
+				try {insLineaAccion = SqlSelects.selectInsLineaAccion(condition);}
 				catch (SQLException e) {e.printStackTrace();}}
 				
-				condition = " where true ";
-				Integer trimestreId = avanceCualitativoAux.getTrimestreId();
-				List<Trimestre> trimestre=null;
-				if (trimestreId!=null) {condition += " and id ='"+trimestreId+"'";
-				try {trimestre = SqlSelects.selectTrimestre(condition);}
-				catch (SQLException e) {e.printStackTrace();}}			
-			
-				contenido += "<div class='content'>"+				
+				Iterator<InsLineaAccion> insLineaAccionIterador = insLineaAccion.iterator();
+				while (insLineaAccionIterador.hasNext()) {
+					InsLineaAccion insLineaAccionAux = insLineaAccionIterador.next();
+				
+		    		condition = " where true and borrado is false";
+		    		Integer insLineaAccionId = insLineaAccionAux.getId();
+					List<AvanceCualitativo> avanceCualitativo=null;					
+					if (insLineaAccionId!=null) condition += " and id ='"+insLineaAccionId+"'";
+					try {avanceCualitativo = SqlSelects.selectAvanceCualitativo(condition);}
+					catch (SQLException e) {e.printStackTrace();}					
 					
-		   							"<h3 style='text-align:center;'><u>SPR-PA-03: Informe Cualitativo de Avance Trimestral</u></h3>"+
-		   							
-		   							"<p id='impresionInstitucion'><strong>Institución: </strong><span>"+ institucion +"</span></p>"		+
-		   							"<p id='impresionAccionesTrimestre'><strong>Acción: </strong><span>"+ catalogo.get(0).getNombre() +"</span></p>"+
-		   							"<p id='impresionTrimestreAño'><strong>Periodo: </strong><span>"+ trimestre.get(0).getDescripcion() + " " + trimestre.get(0).getAnho() + " </span></p>" +
-		   							"<p id='impresionGestionesRealizadas'><strong>Gestiones Realizadas: </strong><span>"+ avanceCualitativoAux.getGestionesRealizadas() +"</span></p>" +
-		   							"<p id='impresionLogrosAlcanzados'><strong>Principales Logros Alcanzados: </strong><span>"+ avanceCualitativoAux.getPrincipalesLogrosAlcanzados() +"</span></p>"	+
-		   							"<p id='impresionLeccionesAprendidas'><strong>Dificultades y Lecciones aprendidas: </strong><span>"+ avanceCualitativoAux.getDificultadesLeccionesAprendidas() +"</span></p>" +
-		   							"<p id='impresionSiguienteTrimestre'><strong>Objetivos del Siguiente Trimestre: </strong><span>"+ avanceCualitativoAux.getObjetivosTrimestre() +"</span></p>"	+
-		   							
-						    	 "</div>";
-    		}
+					Iterator<AvanceCualitativo> avanceCualitativoIterador = avanceCualitativo.iterator();
+					while (avanceCualitativoIterador.hasNext()) {
+						AvanceCualitativo avanceCualitativoAux = avanceCualitativoIterador.next();																																																											
+						
+						condition = " where true ";
+						Integer catalogoAccionId = avanceCualitativoAux.getAccionCatalogoId();
+						List<AccionCatalogo> catalogo=null; 		
+						if (catalogoAccionId!=null) {condition += " and id ='"+catalogoAccionId+"'";
+						try {catalogo = SqlSelects.selectAccionCatalogo(condition);}
+						catch (SQLException e) {e.printStackTrace();}}
+						
+						condition = " where true ";
+						Integer trimestreId = avanceCualitativoAux.getTrimestreId();
+						List<Trimestre> trimestre=null;
+						if (trimestreId!=null) {condition += " and id ='"+trimestreId+"'";
+						try {trimestre = SqlSelects.selectTrimestre(condition);}
+						catch (SQLException e) {e.printStackTrace();}}			
+					
+						contenido += "<div class='content'>"+				
+							
+				   							"<h3 style='text-align:center;'><u>SPR-PA-03: Informe Cualitativo de Avance Trimestral</u></h3>"+
+				   							
+				   							"<p id='impresionInstitucion'><strong>Institución: </strong><span>"+ institucion +"</span></p>"		+
+				   							"<p id='impresionAccionesTrimestre'><strong>Acción: </strong><span>"+ catalogo.get(0).getNombre() +"</span></p>"+
+				   							"<p id='impresionTrimestreAño'><strong>Periodo: </strong><span>"+ trimestre.get(0).getDescripcion() + " " + trimestre.get(0).getAnho() + " </span></p>" +
+				   							"<p id='impresionGestionesRealizadas'><strong>Gestiones Realizadas: </strong><span>"+ avanceCualitativoAux.getGestionesRealizadas() +"</span></p>" +
+				   							"<p id='impresionLogrosAlcanzados'><strong>Principales Logros Alcanzados: </strong><span>"+ avanceCualitativoAux.getPrincipalesLogrosAlcanzados() +"</span></p>"	+
+				   							"<p id='impresionLeccionesAprendidas'><strong>Dificultades y Lecciones aprendidas: </strong><span>"+ avanceCualitativoAux.getDificultadesLeccionesAprendidas() +"</span></p>" +
+				   							"<p id='impresionSiguienteTrimestre'><strong>Objetivos del Siguiente Trimestre: </strong><span>"+ avanceCualitativoAux.getObjetivosTrimestre() +"</span></p>"	+
+				   							
+								    	 "</div><br />";
+		    		}
+				}
+			}
     	}
 				
 		return contenido;
     }
-	
-//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-//    		throws ServletException, IOException {
-//		//Poner el tipo de contenido a la application - pdf
-//		//El navegador va a abrir el documento solo si éste parametro es seteado 
-//		response.setContentType("application/pdf");
-//		//Obtiene el output stream para escribir el objeto PDF        
-//		OutputStream out=response.getOutputStream();
-//		try {
-//			//Obtiene el html que será agregado al documento PDF
-//		  String text = request.getParameter("html");
-//		  if (text == null || text.trim().length() == 0) {
-//		       text = "No has agregado ningún código HTML. Agrega algo para que aparezca aquí...";
-//		  }
-//		  Document document = new Document();            
-//		  PdfWriter.getInstance(document, out);
-//		  document.open();
-//		  
-//		  HTMLWorker htmlWorker = new HTMLWorker(document);
-//		  htmlWorker.parse(new StringReader(text));
-//		  
-//		  document.close();
-//		}
-//		catch (DocumentException exc){
-//		      throw new IOException(exc.getMessage());
-//		}
-//		finally {            
-//		  out.close();
-//		}
-//    }
-    
-//    /**
-//     * Gestiona la generación de archivos pdf a partir de cadenas HTML.
-//     */        
-//	public void createPdf(String html) {    	
-//	    try {
-//    		OutputStream file1 = new FileOutputStream(new File("C:\\Test.pdf"));
-//	        Document document = new Document();       
-//	        PdfWriter.getInstance(document, file1);
-//	        document.open();
-//	        
-//	        //String k = "<html><body> This is my Project </body></html>";
-//	        StringBuilder sb = new StringBuilder();
-//	        sb.append("<div>\n<p align=\"center\">");
-//	        sb.append("<font size=\"5\">");
-//	        sb.append("<b>&nbsp;<font color=\"#32cd32\">My centered Para</font></b>");
-//	        sb.append("</font>");
-//	        sb.append("<font color=\"#32cd32\">&nbsp;</font>");
-//	        sb.append("</p>\n</div>");
-//	         
-//	        HTMLWorker htmlWorker = new HTMLWorker(document);
-//	        htmlWorker.parse(new StringReader(sb.toString()));	 
-//	        
-//	        document.close();
-//	        file1.close();
-//	    } catch (Exception e) {
-//	        e.printStackTrace();
-//	    }
-//    }  
-	
+		
 }
