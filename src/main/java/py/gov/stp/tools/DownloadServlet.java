7403de83ef0622428a439c3a1bcbdeb040e001d9
Alcides Abel Benitez <aabenitez@stp.gov.py>
2017-02-01 15:38:11 -0300

    Tarea #17169: Creado una pagina con el mensaje "Archivo no encontrado en el servidor" para la excepción.

--- src/main/java/py/gov/stp/tools/DownloadServlet.java
+++ src/main/java/py/gov/stp/tools/DownloadServlet.java
@@ -2,6 +2,7 @@ package py.gov.stp.tools;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -21,46 +22,51 @@ public class DownloadServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
 		
 		// lee el archivo del path absoluto
-	    String filePath = getServletContext().getInitParameter("file-upload") + File.separator + request.getParameter("urlDocumento");
-	    File downloadFile = new File(filePath);
-	    FileInputStream inStream = new FileInputStream(downloadFile);
+	    String filePath = getServletContext().getInitParameter("file-upload") + File.separator + request.getParameter("urlDocumento");	    
+	    try{
+	    	File downloadFile = new File(filePath);
+	    	FileInputStream inStream = new FileInputStream(downloadFile);
 	     
-	    // if you want to use a relative path to context root:
-//	    String relativePath = getServletContext().getRealPath("");
-//	    System.out.println("relativePath = " + relativePath);
-	     
-	    // obtiene ServletContext
-	    ServletContext context = getServletContext();
-	     
-	    // obtiene el tipo de MIME 
-	    String mimeType = context.getMimeType(filePath);
-	    if (mimeType == null) {        
-	        // setea en tipo binario el MIME cuando el tomcat no identifica el tipo de aplicación.
-	        mimeType = "application/octet-stream";
+		    // if you want to use a relative path to context root:
+	//	    String relativePath = getServletContext().getRealPath("");
+	//	    System.out.println("relativePath = " + relativePath);
+		     
+		    // obtiene ServletContext
+		    ServletContext context = getServletContext();
+		     
+		    // obtiene el tipo de MIME 
+		    String mimeType = context.getMimeType(filePath);
+		    if (mimeType == null) {        
+		        // setea en tipo binario el MIME cuando el tomcat no identifica el tipo de aplicación.
+		        mimeType = "application/octet-stream";
+		    }
+		    System.out.println("MIME type: " + mimeType);
+		     
+		    // modifica la respuesta
+		    response.setContentType(mimeType);
+		    response.setContentLength((int) downloadFile.length());
+		     
+		    // fuerza la descarga
+		    String headerKey = "Content-Disposition";
+		    String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+		    response.setHeader(headerKey, headerValue);
+		     
+		    // obtiene la respuesta en output stream
+		    OutputStream outStream = response.getOutputStream();
+		     
+		    byte[] buffer = new byte[15360];// 15 MB MaxFileSize del UploadServlet
+		    int bytesRead = -1;
+		     
+		    while ((bytesRead = inStream.read(buffer)) != -1) {
+		        outStream.write(buffer, 0, bytesRead);
+		    }
+		     
+		    inStream.close();
+		    outStream.close();
+	    } catch (FileNotFoundException fnfe){
+	    	response.sendRedirect("/tablero/archivo_no_encontrado.jsp");  	   
 	    }
-	    System.out.println("MIME type: " + mimeType);
-	     
-	    // modifica la respuesta
-	    response.setContentType(mimeType);
-	    response.setContentLength((int) downloadFile.length());
-	     
-	    // fuerza la descarga
-	    String headerKey = "Content-Disposition";
-	    String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
-	    response.setHeader(headerKey, headerValue);
-	     
-	    // obtiene la respuesta en output stream
-	    OutputStream outStream = response.getOutputStream();
-	     
-	    byte[] buffer = new byte[15360];// 15 MB MaxFileSize del UploadServlet
-	    int bytesRead = -1;
-	     
-	    while ((bytesRead = inStream.read(buffer)) != -1) {
-	        outStream.write(buffer, 0, bytesRead);
-	    }
-	     
-	    inStream.close();
-	    outStream.close();     
+	    
 	}
 
 }
